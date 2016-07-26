36d11365fdc33af7b9b295c29991f183429ae4a2
bluepun <aschiefe@htl-steyr.ac.at>
2016-07-26 11:58:36 +0200

    follow.js
    line 21:
    dataInterval - This allows you to change the frequency of points returned. CAUTION: This value is usually set automatically to work with your chosen duration. If set to an incompatible interval, the graphs will return no data.
    - Deleted initial value

--- follows.js
+++ follows.js
@@ -17,9 +17,9 @@
 	var defaultKey		= 'qaD0dFAoHM7uWgaU0CDkFi1npsdyu9B8LG83WFNZ19XHcbAQ', // Unique master Xively API key to be used as a default
 		defaultFeeds	= [1279051540], // Comma separated array of Xively Feed ID numbers
 		applicationName	= 'Visualisierung FBH-Anhänger', // Replaces Xively logo in the header
-		dataDuration	= '30days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
-		dataInterval	= 10800, // Default interval for data to be displayed (in seconds)
-		dataColor		= 'ff0000', // CSS HEX value of color to represent data (omit leading #)
+		dataDuration	= '20days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
+		dataInterval	= , // Default interval for data to be displayed (in seconds)
+		dataColor		= '0A1922', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 1; // To hide input form use value of 1, otherwise set to 0
 
 // Function Declarations
