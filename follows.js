caa991b558d80692f6cbb1fd6f4c2a55e45ecc56
bluepun <aschiefe@htl-steyr.ac.at>
2016-08-02 08:28:24 +0200

    Simple correction
    line 23
    -in the last update we released a new variable, but didn't change the above ';' to an ','

--- follows.js
+++ follows.js
@@ -20,7 +20,7 @@
 		dataDuration	= '20days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 1800, // Default interval for data to be displayed (in seconds)
 		dataColor	= '0A1922', // CSS HEX value of color to represent data (omit leading #)
-		hideForm	= 1; // To hide input form use value of 1, otherwise set to 0
+		hideForm	= 1, // To hide input form use value of 1, otherwise set to 0
 		state 		= 0; // First graph is a scatterplot, second one is a linegraph(Soll/Ist)
 		
 // Function Declarations
